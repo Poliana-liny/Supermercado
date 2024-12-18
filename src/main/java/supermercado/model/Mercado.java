786a3dcac2648fb7bdde1f53e94213b166773106
Poliana Liny <polianacaroliny92@gmail.com>
2024-12-18 18:23:22 -0300

    initial commit

---
+++ src/main/java/supermercado/model/Mercado.java
@@ -0,0 +1,89 @@
+package supermercado.model;
+
+import java.util.ArrayList;
+
+public class Mercado {
+    private int codigo;
+    private String nome;
+    private ArrayList<Categoria> categorias;
+
+    public Mercado(String nome){
+        this.setCategorias(new ArrayList<>());
+
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(ArrayList<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    public void adicionarCategoria(Categoria categoria){
+        categorias.add(categoria);
+    }
+
+    public void removerCategoria(int categoria){
+        System.out.println(categorias.get(codigo).getNome()+"Foi removido com sucesso!");
+        categorias.remove(codigo);
+    }
+
+    public void apresentarCategorias(){
+        for (int i=0; i<categorias.size();i++){
+          System.out.println("Codigo: "+i+", Nome da Categoria: "+categorias.get(i).getNome());
+        }
+    }
+
+    public void apresentarItens(){
+        for (int i=0; i<categorias.size();i++){
+            ArrayList<Item> itens = categorias.get(i).getItens();
+
+            System.out.println("Categoria: "+i);
+            for (int j=0; j< itens.size();j++){
+                System.out.println("Nome: "+itens.get(j).getNome()+", Preço: "+itens.get(1).getPreco());
+            }
+
+            System.out.println("");
+        }
+    }
+
+    public void apresentarCategoriaPorCodigo(int codigo){
+        for (int i=0; i<categorias.size();i++){
+            if(codigo == 0){
+                for(int j=0; j<categorias.get(0).getItens().size(); j++){
+                    System.out.println(categorias.get(i).getItens().get(j).getNome());
+                }
+                i = categorias.size();
+            } else if (codigo == 1){
+                for(int j=0; j<categorias.get(1).getItens().size(); j++) {
+                    System.out.println(categorias.get(i).getItens().get(j).getNome());
+                }
+                    i = categorias.size();
+            }else if (codigo == 2){
+                    for(int j=0; j<categorias.get(2).getItens().size(); j++){
+                        System.out.println(categorias.get(i).getItens().get(j).getNome());
+                }
+                    i = categorias.size();
+
+            }
+
+        }
+    }
+}
